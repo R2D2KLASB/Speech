6cd9b4fe6fb8b6037dd29c11b44f1affefe9750e
marhotiewer <marhotiewer@protonmail.com>
2022-03-21 11:04:20 +0100

    Added defines to easily change recording quality and format

--- portaudio_test/portaudio_test.cpp
+++ portaudio_test/portaudio_test.cpp
@@ -3,27 +3,37 @@
 #include <stdint.h>
 #include "portaudio.h"
 
-typedef struct {
-    int     frameIndex = 0;
-    int     maxFrameIndex;
-    float   *recordedSamples;
-} PaTestData;
+#define SAMPLE_SILENCE  (0) // (0.0f) for float
+#define PA_SAMPLE_TYPE  paInt16
+#define C_SAMPLE_TYPE   int16_t
+#define BITS_PER_SAMPLE 16
+#define SAMPLE_RATE     16000
+#define BUFFER_SIZE     512
+#define NUM_CHANNELS    1
+#define AUDIO_FORMAT    1
+#define NUM_SECONDS     5
 
 typedef struct {
-    uint8_t     chunkID[4] = {'R', 'I', 'F', 'F'};
-    uint32_t    chunkSize;
-    uint8_t     format[4] = {'W', 'A', 'V', 'E'};
-    uint8_t     subchunk1ID[4] = {'f', 'm', 't', ' '};
-    uint32_t    subchunk1Size = 16;
-    uint16_t    audioFormat = 3;
-    uint16_t    numChannels = 1;
-    uint32_t    sampleRate = 44100;
-    uint32_t    byteRate = 176400;
-    uint16_t    blockAlign = 4;
-    uint16_t    bitsPerSample = 32;
-    uint8_t     subchunk2ID[4] = {'d', 'a', 't', 'a'};
-    uint32_t    subchunk2Size;
+    uint8_t     chunkID[4]      = {'R', 'I', 'F', 'F'};
+    uint32_t    chunkSize;      // size of audio + sizeof(WAV_HEADER) - 8
+    uint8_t     format[4]       = {'W', 'A', 'V', 'E'};
+    uint8_t     subchunk1ID[4]  = {'f', 'm', 't', ' '};
+    uint32_t    subchunk1Size   = 16;
+    uint16_t    audioFormat     = AUDIO_FORMAT;
+    uint16_t    numChannels     = NUM_CHANNELS;
+    uint32_t    sampleRate      = SAMPLE_RATE;
+    uint32_t    byteRate        = (SAMPLE_RATE * BITS_PER_SAMPLE * NUM_CHANNELS) / 8;
+    uint16_t    blockAlign      = (BITS_PER_SAMPLE * NUM_CHANNELS) / 8;
+    uint16_t    bitsPerSample   = BITS_PER_SAMPLE;
+    uint8_t     subchunk2ID[4]  = {'d', 'a', 't', 'a'};
+    uint32_t    subchunk2Size;  // size of audio + sizeof(WAV_HEADER) - 44
 } WAV_HEADER;
+
+typedef struct {
+    int             frameIndex = 0;
+    int             maxFrameIndex;
+    C_SAMPLE_TYPE   *recordedSamples;
+} PaTestData;
 
 static int recordCallback(const void *inputBuffer, void *outputBuffer,
                         unsigned long framesPerBuffer,
@@ -32,8 +42,8 @@ static int recordCallback(const void *inputBuffer, void *outputBuffer,
                         void *userData)
 {
     PaTestData *data = (PaTestData*)userData;
-    const float *rptr = (const float*)inputBuffer;
-    float *wptr = &data->recordedSamples[data->frameIndex];
+    const C_SAMPLE_TYPE *rptr = (const C_SAMPLE_TYPE*)inputBuffer;
+    C_SAMPLE_TYPE *wptr = &data->recordedSamples[data->frameIndex];
     unsigned long framesLeft = data->maxFrameIndex - data->frameIndex;
     long framesToCalc, i;
     int finished;
@@ -48,12 +58,14 @@ static int recordCallback(const void *inputBuffer, void *outputBuffer,
     }
     if(inputBuffer == NULL) {
         for(i = 0; i < framesToCalc; i++) {
-            *wptr++ = (0.0f);
+            *wptr++ = SAMPLE_SILENCE;
+            if(NUM_CHANNELS == 2) { *wptr++ = SAMPLE_SILENCE; }
         }
     }
     else {
         for(i = 0; i < framesToCalc; i++) {
             *wptr++ = *rptr++;
+            if(NUM_CHANNELS == 2) { *wptr++ = *rptr++; }
         }
     }
     data->frameIndex += framesToCalc;
@@ -68,9 +80,9 @@ int main() {
     int numBytes;
     FILE *sample;
 
-    data.maxFrameIndex = 5 * 44100;
-    numBytes = data.maxFrameIndex * sizeof(float);
-    data.recordedSamples = (float*)malloc(numBytes);
+    data.maxFrameIndex = NUM_SECONDS * SAMPLE_RATE * NUM_CHANNELS;
+    numBytes = data.maxFrameIndex * sizeof(C_SAMPLE_TYPE);
+    data.recordedSamples = (C_SAMPLE_TYPE*)malloc(numBytes);
 
     wav.chunkSize = numBytes + sizeof(WAV_HEADER) - 8;
     wav.subchunk2Size = numBytes + sizeof(WAV_HEADER) - 44;
@@ -78,12 +90,12 @@ int main() {
     Pa_Initialize();
 
     inputParameters.device = Pa_GetDefaultInputDevice();
-    inputParameters.channelCount = 1;
-    inputParameters.sampleFormat = paFloat32;
+    inputParameters.channelCount = NUM_CHANNELS;
+    inputParameters.sampleFormat = PA_SAMPLE_TYPE;
     inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultLowInputLatency;
     inputParameters.hostApiSpecificStreamInfo = NULL;
 
-    Pa_OpenStream(&stream, &inputParameters, NULL, 44100, 512, paClipOff, recordCallback, &data);
+    Pa_OpenStream(&stream, &inputParameters, NULL, SAMPLE_RATE, BUFFER_SIZE, paClipOff, recordCallback, &data);
     Pa_StartStream(stream);
 
     while(Pa_IsStreamActive(stream)) {
@@ -93,7 +105,7 @@ int main() {
 
     sample = fopen("recording.wav", "wb");
     fwrite(&wav, sizeof(wav), 1, sample);
-    fwrite(data.recordedSamples, sizeof(float), data.maxFrameIndex, sample);
+    fwrite(data.recordedSamples, sizeof(C_SAMPLE_TYPE), data.maxFrameIndex, sample);
     fclose(sample);
     printf("wrote data to 'recording.wav'\n");
 
