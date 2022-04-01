7557d91ec1db730d8b1c4b8074de656dd9d2ad25
KyrillW <KyrillW@hotmail.com>
2022-04-01 18:50:35 +0200

    Update convert.py

--- gcode_test/convert.py
+++ gcode_test/convert.py
@@ -1,12 +1,29 @@
 import sys
 
-a_file = open(sys.argv[1], "r")
-output = "{"
-for line in a_file:
-    if line.strip()[0:3] == "G00":
-        x = line.strip().find('X')
-        y = line.strip().find('Y')
-        output += "{"
-        for
+with open(sys.argv[1], "r") as a_file, open("gcode_struct.txt", "a") as new_file:
+    output = "{"
+    for line in a_file:
+        if len(line.strip()) == 1:
+            new_file.write(f"{line.strip()}\n\n")
+        elif line.strip()[0:3] == "G00":
+            x = line.strip().find('X')
+            y = line.strip().find('Y')
+            if line.strip()[x+1] == '0' and line.strip()[y+1] == '0':
+                continue
+            output += "{"
+            output += line.strip()[x+1:y-1]
+            output += ", "
+            output += line.strip()[y+1:len(line.strip())]
+            output += ", \"G00\"},\n"
+        elif line.strip()[0:3] == "G01":
+            x = line.strip().find('X')
+            y = line.strip().find('Y')
+            output += "{"
+            output += line.strip()[x+1:y-1]
+            output += ", "
+            output += line.strip()[y+1:len(line.strip())]
+            output += ", \"G01\"},\n"
 
-input("saus")
+    output = output[:-2] + "},\n\n"
+    new_file.write(output)
+    new_file.close()
