27b52e39aa70438112eae389de81a82b8742102a
marhotiewer <marhotiewer@protonmail.com>
2022-06-21 10:28:12 +0000

    matrix integration

--- zeeslag/zeeslag/src/listener.cpp
+++ zeeslag/zeeslag/src/listener.cpp
@@ -5,22 +5,47 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+#include "led-matrix.h"
+#include "signal.h"
+
 using std::placeholders::_1;
+using rgb_matrix::RGBMatrix;
+using rgb_matrix::Canvas;
 
 class PublishingSubscriber: public rclcpp::Node {
   public:
     PublishingSubscriber(): Node("publishing_subscriber") {
       subscription_ = this->create_subscription<std_msgs::msg::String>("sub", 10, std::bind(&PublishingSubscriber::topic_callback, this, _1));
       publisher_ = this->create_publisher<std_msgs::msg::String>("pub",10);
+
+      RGBMatrix::Options my_defaults;
+      my_defaults.hardware_mapping = "regular";
+      my_defaults.chain_length = 1;
+      my_defaults.rows = 16;
+      my_defaults.cols = 32;
+      my_defaults.show_refresh_rate = true;
+      my_defaults.disable_hardware_pulsing = 1;
+      my_defaults.parallel = 1;
+      rgb_matrix::RuntimeOptions runtime_defaults;
+      runtime_defaults.gpio_slowdown = 4;
+
+      canvas = RGBMatrix::CreateFromOptions(my_defaults, runtime_defaults);
+    }
+    ~PublishingSubscriber() {
+      delete canvas;
     }
   private:
     void topic_callback(const std_msgs::msg::String::SharedPtr msg) const {
       auto message = std_msgs::msg::String();
       message.data = "I heard " + msg->data;
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
+
+      canvas->Fill(0,0,255);
     }
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    Canvas* canvas;
 };
 
 int main(int argc, char* argv[]) {
