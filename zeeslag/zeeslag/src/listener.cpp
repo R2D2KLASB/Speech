0c95fa972584743dde01286b53844c446e57894c
marhotiewer <marhotiewer@protonmail.com>
2022-06-21 09:27:47 +0000

    pubsub node

--- zeeslag/zeeslag/src/listener.cpp
+++ zeeslag/zeeslag/src/listener.cpp
@@ -1,25 +1,31 @@
+#include <functional>
 #include <memory>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+
 using std::placeholders::_1;
 
-class MinimalSubscriber: public rclcpp::Node {
+class PublishingSubscriber: public rclcpp::Node {
   public:
-    MinimalSubscriber(): Node("matrix_control") {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    PublishingSubscriber(): Node("publishing_subscriber") {
+      subscription_ = this->create_subscription<std_msgs::msg::String>("sub", 10, std::bind(&PublishingSubscriber::topic_callback, this, _1));
+      publisher_ = this->create_publisher<std_msgs::msg::String>("pub",10);
     }
   private:
     void topic_callback(const std_msgs::msg::String::SharedPtr msg) const {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+      auto message = std_msgs::msg::String();
+      message.data = "I heard " + msg->data;
+      publisher_->publish(message);
     }
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 };
 
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<PublishingSubscriber>());
   rclcpp::shutdown();
   return 0;
 }
