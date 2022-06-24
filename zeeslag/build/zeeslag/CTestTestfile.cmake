95760c4b271a8ae6758cbb2876af5200111e0c8b
marhotiewer <marhotiewer@protonmail.com>
2022-06-24 13:49:59 +0000

    small tests

--- zeeslag/build/zeeslag/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Speech/zeeslag/zeeslag
-# Build directory: /home/ubuntu/Speech/zeeslag/build/zeeslag
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
